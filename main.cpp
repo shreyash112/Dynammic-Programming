1c4382c6e22e56771e99185bdef5b5590c209d33
Shreyash <37359296+shreyash112@users.noreply.github.com>
2019-11-17 12:50:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,20 +6,27 @@
 #include <math.h>
 #include<algorithm>
 using namespace std;
+
+//Protype functions
 int mem_rod (int price[], int n);
 int cutRod(int price[], int n,int r[]);
+
+//Function to measure the maximum value comparing to the previous max_val
 int max(int a, int b)
 {
     return (a > b)? a : b;
 }
 
+//Function to store the repeating the elements in an array.
 int mem_rod(int price[], int n)
 {
     int rod1[n];
     std :: fill_n(rod1,n,NULL);
-
     return cutRod(price,n,rod1);
 }
+
+//Recurssive function to calculate the maximum revenue generated using dynamic
+//Programming approach.
 int cutRod(int price[], int n,int r[])
 {
     if (r[n-1] > 0)
@@ -45,6 +52,8 @@ int cutRod(int price[], int n,int r[])
         return max_val;
     }
 }
+
+//Function to generate the prices according to formula given.
 void gen_price(int price1[],int rod_size)
 {
     int j = 1;
@@ -67,6 +76,7 @@ void gen_price(int price1[],int rod_size)
         j++;
     }
 }
+
 int main()
 {
     int rod_size;
@@ -79,11 +89,11 @@ int main()
         cout << price1[i] << " ";
     }
     clock_t t;
-    t = clock();
+    t = clock();//Start the clock to measure the excetuion time of the program 
     cout <<'\n';
     int rod = mem_rod(price1,rod_size);
     cout << "\nMaximum Revenue: "<< rod;
-    t = clock() - t;
+    t = clock() - t; //Stop the clock 
     printf ("\nIt took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
     return 0;
 }
